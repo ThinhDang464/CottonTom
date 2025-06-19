2f85957178a3773a11368f7ca23ec931f762219d
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-06-19 16:59:41 +1000

    nav link

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,8 +1,28 @@
 import { assets } from "../assets/assets.ts";
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="logo" />
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col gap-1 items-center">
+          <p>HOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/collection" className="flex flex-col gap-1 items-center">
+          <p>COLLECTION</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col gap-1 items-center">
+          <p>ABOUT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col gap-1 items-center">
+          <p>CONTACT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+      </ul>
     </div>
   );
 };
