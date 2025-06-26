1c13a0a142fc6a1cbc2138caf2ee3e997491093b
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-06-26 13:56:23 +1000

    left side filter collection page

--- frontend/src/pages/Collection.tsx
+++ frontend/src/pages/Collection.tsx
@@ -1,5 +1,76 @@
+import { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
+
 const Collection = () => {
-  return <div></div>;
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 border-t">
+      {/*Filter options*/}
+      <div className="min-w-60">
+        <p
+          onClick={() => {
+            setShowFilter(!showFilter);
+          }}
+          className="my-2 text-xl flex items-center cursor-pointer gap-2"
+        >
+          FILTERS
+          <img
+            className={`h-3 sm:hidden transition-transform duration-300 ${
+              showFilter ? "rotate-90" : ""
+            }`}
+            src={assets.dropdown_icon}
+          />
+        </p>
+
+        {/*Category filters */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block`} //sm block override hidden on screen small or wider -> filter will not show on mobile screen at this point
+        >
+          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Men"} /> Men
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Women"} /> Women
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Kids"} /> Kids
+            </p>
+          </div>
+        </div>
+        {/*Sub-cat filters */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 my-5 ${
+            showFilter ? "" : "hidden"
+          } sm:block`} //sm block override hidden on screen small or wider -> filter will not show on mobile screen at this point
+        >
+          <p className="mb-3 text-sm font-medium">TYPE</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Topwear"} />
+              Topwear
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Bottomwear"} />
+              Bottomwear
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Winterwear"} />
+              Winterwear
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/*Right side */}
+      <div></div>
+    </div>
+  );
 };
 
 export default Collection;
