896d56daf0d1ff7dd7c48ac4a728a4a6316a18ab
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-06-28 20:09:07 +1000

    UI for search bar

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,37 @@
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
+
+const SearchBar = () => {
+  const { search, setSearch, showSearch, setShowSearch } =
+    useContext(ShopContext);
+
+  return showSearch ? (
+    <div className="border-t bg-gray-50 text-center">
+      {/*inline flex make the container flex but not take up whole width only what the child are -> we set width to 3/4 of  parent div here */}
+      {/*without items-center -> flex use align items stretch for vertical -> off ratio of image cause it stretch to fill container based on tallest child */}
+      <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 ">
+        <input
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value); //controlled variable
+          }}
+          className="flex-1 outline-none bg-inherit text-sm"
+          type="text"
+          placeholder="Search"
+        />
+        <img className="w-4" src={assets.search_icon} alt="" />
+      </div>
+      <img
+        onClick={() => {
+          setShowSearch(false);
+        }}
+        className="inline w-3 cursor-pointer"
+        src={assets.cross_icon}
+        alt=""
+      />
+    </div>
+  ) : null;
+};
+
+export default SearchBar;
--- frontend/src/context/ShopContext.tsx
+++ frontend/src/context/ShopContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 //store state variables to be used
@@ -12,10 +12,16 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
+  const [search, setSearch] = useState(""); //use search feature across different components
+  const [showSearch, setShowSearch] = useState(false);
   const value = {
     products,
     currency,
     delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
 
   return (
