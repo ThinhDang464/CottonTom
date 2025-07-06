63ceb1a3ae6ccd3e41e83f6f22ada05dee1576c5
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-07-06 15:29:23 +1000

    finish about us page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,4 +1,5 @@
 import { assets } from "../assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
 import Title from "../components/Title";
 
 const About = () => {
@@ -27,8 +28,48 @@ const About = () => {
             deleniti ipsam corrupti tempore expedita culpa earum aperiam? Eius
             illo eos dolore, quia similique quod ratione rem est ducimus vero!
           </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+            reiciendis asperiores harum, in nostrum minima molestiae, omnis,
+            perspiciatis alias adipisci
+          </p>
         </div>
       </div>
+      <div className="text-xl py-4">
+        <Title text1={"WHY"} text2={"COTTONTOM"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance: </b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+            corporis, temporibus eaque animi labore reiciendis! Id molestiae
+            dignissimos voluptate! Dicta at tempore soluta eum porro aliquam
+            dolores numquam, inventore hic.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience: </b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+            corporis, temporibus eaque animi labore reiciendis! Id molestiae
+            dignissimos voluptate! Dicta at tempore soluta eum porro aliquam
+            dolores numquam, inventore hic.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Customer Focused: </b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+            corporis, temporibus eaque animi labore reiciendis! Id molestiae
+            dignissimos
+          </p>
+        </div>
+      </div>
+
+      {/*Newsletter sub box already created*/}
+      <NewsLetterBox />
     </div>
   );
 };
