9a21d904b1e9fb8653f877c40b6ec489526d7d34
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-07-06 12:18:30 +1000

    about us UI

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,5 +1,36 @@
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
+
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+
+        {/*direction is col so justify center align center vertically */}
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            ducimus distinctio ad itaque cupiditate veniam error voluptatibus
+            placeat totam velit ea, incidunt eum omnis amet odio similique,
+            porro excepturi dicta.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugit
+            deleniti ipsam corrupti tempore expedita culpa earum aperiam? Eius
+            illo eos dolore, quia similique quod ratione rem est ducimus vero!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,9 +4,16 @@ const Login = () => {
   const [currentState, setCurrentState] = useState("Sign Up");
 
   //set as async cause it will deal with databae operation later on
-  const onSubmitHandler = async (event) => {};
+  const onSubmitHandler = async (event) => {
+    event.preventDefault(); //no reloading when submit
+  };
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <form
+      onSubmit={() => {
+        onSubmitHandler;
+      }}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
       {/*inline flex turn p into inline to have hr next to it */}
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
