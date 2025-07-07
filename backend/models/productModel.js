2fdb0fd11fb927f2c9d0a413a96ab97fe803812c
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-07-07 16:21:40 +1000

    create product Model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,2 +1,20 @@
 //Schema for product to store on Mongo
 import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true }, //required -> need to be saved in database
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: Array, required: true },
+  category: { type: String, required: true },
+  subCategory: { type: String, required: true },
+  sizes: { type: Array, required: true },
+  bestSeler: { type: Boolean },
+  date: { type: Number, required: true },
+});
+
+//when product model is already available, then that model will be used insstead of craeting multple times
+const productModel =
+  mongoose.models.product || mongoose.model("product", productSchema);
+
+export default productModel;
