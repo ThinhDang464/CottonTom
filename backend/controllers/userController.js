5a8b211c139e2b7defd41410e190f0ae832c60b2
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-07-08 16:53:48 +1000

    implement userController Login function + generate token for valid user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,34 @@ const createToken = (id) => {
 };
 
 //Route for user login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password); //user.password is the one saved in DB(hashed)
+
+    if (isMatch) {
+      //generate valid token
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      //password do not match
+      res.json({ success: false, message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //Route for user sign up
 const registerUser = async (req, res) => {
