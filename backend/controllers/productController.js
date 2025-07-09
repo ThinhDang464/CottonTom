207ca7457cf11a498c5c1ed630fc1115514996b5
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-07-09 13:36:40 +1000

    addProduct implementation with cloudinary and multer + mongo + route is not protected yet

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 import productModel from "../models/productModel.js";
+import { v2 as cloudinary } from "cloudinary";
 //to manage product
 
 //use Thunder CLient to test, form data enabled
@@ -22,19 +23,67 @@ const addProduct = async (req, res) => {
     const image3 = req.files.imag3 && req.files.image3[0];
     const image4 = req.files.imag4 && req.files.image4[0];
 
-    console.log(
-      name,
-      description,
-      price,
-      category,
-      subCategory,
-      sizes,
-      bestseller
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    ); //only store available images that user upload to array
+
+    /*---------------------------------------------CLOUDINARY------------------------------------------------------------- */
+    //upload images to cloudinary and get permanent url to store in images url array
+    //Promise.all takes an aray of promise as parameter (since async with map we create an array [Promise, Promise, Promise] before it is resolved)
+    //Promise.all turn array of promise into one single promise represent entire batch
+    //type of data return back to imagesURL depends on what each promise item resolves to - array of string url
+    //promise all runs all the input promises concurrently
+    /*WITH TRADITIONAL SEQUENTIAL UPLOAD WITH FOR LOOP:
+    let urls = [];
+    for (const image of images) {
+      // The 'await' here PAUSES the entire loop for each upload
+      let result = await cloudinary.uploader.upload(image.path);
+      urls.push(result.secure_url);
+    }
+
+    image upload then pause and wait for upload done before second image start upload -> inefficent
+
+    WITH PROMISE ALL:
+    map go through all upload and call upload for eachone roughly same time -> collect 4 pending promises into arra
+    await Promise all pause the function wait for all concurrent running uploads to complete instead of each single one
+    */
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        }); //path is a property of item in images aray, result now contains all info ab upload to Cloudinary, extract secure_url for permanent link
+        return result.secure_url;
+      })
     );
 
-    console.log(image1, image2, image3, image4); // {fieldname:image 1, originalname:,encoding, etc} an object has info about the file uploaded
+    // console.log(
+    //   name,
+    //   description,
+    //   price,
+    //   category,
+    //   subCategory,
+    //   sizes,
+    //   bestseller
+    // );
 
-    res.json({});
+    // console.log(images); // [{fieldname:image 1, originalname:,encoding, etc},...] an array of object has info about the file uploaded
+
+    /*----------------------------------------------------------SAVE UPLOADED PRODUCT TO MONGODB---------------------------------------------- */
+    const productData = {
+      name,
+      description,
+      category,
+      price: Number(price), //price from form data is string
+      subCategory,
+      bestseller: bestseller === "true" ? true : false, //string to boolean
+      sizes: JSON.parse(sizes), //sizes send as string from frontend here convert sizes from string to aray
+      image: imagesUrl,
+      date: Date.now(),
+    };
+
+    const product = new productModel(productData);
+    await product.save(); //dont use any properties from product now so no need another const like userController to generate token
+    res.json({ success: true, message: "Product added to DB" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
