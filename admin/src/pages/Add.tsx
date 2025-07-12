f49b417bd4abe71ec0402ebc74589da0e9bd9e48
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-07-12 19:26:56 +1000

    set up state for controlled variable

--- admin/src/pages/Add.tsx
+++ admin/src/pages/Add.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import { assets } from "../assets/assets";
 
 const Add = () => {
+  const [image1, setImage1] = useState(false);
+  const [image2, setImage2] = useState(false);
+  const [image3, setImage3] = useState(false);
+  const [image4, setImage4] = useState(false);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("Men");
+  const [subCategory, setSubCategory] = useState("Topwear");
+  const [price, setPrice] = useState("");
+  const [bestSeller, setBestSeller] = useState(false);
+  const [sizes, setSizes] = useState([]);
   return (
     <form className="flex flex-col w-full items-start gap-3">
       <div>
@@ -8,35 +21,55 @@ const Add = () => {
         <div className="flex gap-2">
           <label htmlFor="image1">
             <img
-              src={assets.upload_area}
+              src={!image1 ? assets.upload_area : URL.createObjectURL(image1)}
               alt=""
               className="cursor-pointer w-20"
             />
-            <input type="file" id="image1" hidden />
+            <input
+              onChange={(e) => setImage1(e.target.files[0])}
+              type="file"
+              id="image1"
+              hidden
+            />
           </label>
           <label htmlFor="image2">
             <img
-              src={assets.upload_area}
+              src={!image2 ? assets.upload_area : URL.createObjectURL(image2)}
               alt=""
               className="cursor-pointer w-20"
             />
-            <input type="file" id="image2" hidden />
+            <input
+              onChange={(e) => setImage2(e.target.files[0])}
+              type="file"
+              id="image2"
+              hidden
+            />
           </label>
           <label htmlFor="image3">
             <img
-              src={assets.upload_area}
+              src={!image3 ? assets.upload_area : URL.createObjectURL(image3)} //display image when uploaded
               alt=""
               className="cursor-pointer w-20"
             />
-            <input type="file" id="image3" hidden />
+            <input
+              onChange={(e) => setImage3(e.target.files[0])}
+              type="file"
+              id="image3"
+              hidden
+            />
           </label>
           <label htmlFor="image4">
             <img
-              src={assets.upload_area}
+              src={!image4 ? assets.upload_area : URL.createObjectURL(image4)}
               alt=""
               className="cursor-pointer w-20"
             />
-            <input type="file" id="image4" hidden />
+            <input
+              onChange={(e) => setImage4(e.target.files[0])}
+              type="file"
+              id="image4"
+              hidden
+            />
           </label>
         </div>
       </div>
@@ -45,6 +78,8 @@ const Add = () => {
       <div className="w-full">
         <p className="mb-2">Product Name</p>
         <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
           className="w-full max-w-[500px] px-3 py-2" //border customization for all input in index.css save time
           type="text"
           placeholder="Type here"
@@ -54,6 +89,8 @@ const Add = () => {
       <div className="w-full">
         <p className="mb-2">Product Description</p>
         <textarea
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
           className="w-full max-w-[500px] px-3 py-2" //border customization for all input in index.css save time
           placeholder="Write content here"
           required
@@ -62,7 +99,10 @@ const Add = () => {
       <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
         <div>
           <p className="mb-2">Product Category</p>
-          <select className="w-full px-2 py-2">
+          <select
+            onChange={(e) => setCategory(e.target.value)}
+            className="w-full px-2 py-2"
+          >
             <option value="Men">Men</option>
             <option value="Women">Women</option>
             <option value="Kids">Kids</option>
@@ -71,7 +111,10 @@ const Add = () => {
 
         <div>
           <p className="mb-2">Product Sub-category</p>
-          <select className="w-full px-2 py-2">
+          <select
+            onChange={(e) => setSubCategory(e.target.value)}
+            className="w-full px-2 py-2"
+          >
             <option value="Topwear">Topwear</option>
             <option value="Bottomwear">Bottomwear</option>
             <option value="Winterwear">Winterwear</option>
@@ -81,6 +124,8 @@ const Add = () => {
         <div>
           <p className="mb-2">Product Price</p>
           <input
+            onChange={(e) => setPrice(e.target.value)}
+            value={price}
             className="w-full px-3 py-2 sm:w-[120px]"
             type="number"
             placeholder="25"
@@ -91,7 +136,15 @@ const Add = () => {
       <div>
         <p className="mb-2">Product Sizes</p>
         <div className="flex gap-3">
-          <div>
+          <div
+            onClick={() =>
+              setSizes((prev) =>
+                prev.includes("S")
+                  ? prev.filter((item) => item !== "S")
+                  : [...prev, "S"]
+              )
+            }
+          >
             <p className="bg-slate-200 px-3 py-1 cursor-pointer">S</p>
           </div>
 
