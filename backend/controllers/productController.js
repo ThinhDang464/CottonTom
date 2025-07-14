19bf9b0d8f2e9bc773328ced73811727c1933deb
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-07-14 12:36:02 +1000

    remove product functionality + UI

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -96,7 +96,7 @@ const addProduct = async (req, res) => {
 const listProduct = async (req, res) => {
   try {
     const products = await productModel.find({});
-    res.json({ sucess: true, products });
+    res.json({ success: true, products });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
